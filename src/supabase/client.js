65ea91264525aee1b758860d0d05c780dcc38fe8
Abdulrahman Adekeye <58373414+Abdulrahaman1000@users.noreply.github.com>
2025-07-17 17:03:42 -0700

    evironment-variable

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,7 +1,11 @@
 // src/supabase/client.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uyjjcohrsnujjfnpzatk.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5ampjb2hyc251ampmbnB6YXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MDQ2NzUsImV4cCI6MjA2ODI4MDY3NX0.llAaB0nFLR3GiddKGZevsBJCGHi1dfwUF-0aXqKL2Jg'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
